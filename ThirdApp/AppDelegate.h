af0cc825b5eca32802015ccefd5bd932ae933e64
zkropotkine <s.daniel.rodriguez@gmail.com>
2014-10-13 22:12:13 -0500

    Initial Commit

---
+++ ThirdApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdApp
+//
+//  Created by Daniel Rodriguez on 10/13/14.
+//  Copyright (c) 2014 Daniel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThirdApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThirdApp
+//
+//  Created by Daniel Rodriguez on 10/13/14.
+//  Copyright (c) 2014 Daniel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
